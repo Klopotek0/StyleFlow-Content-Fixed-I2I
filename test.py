0446762a86eb3ec6a725ee3a4466345409fb3409
Jakub Kłopotek Główczewski <115160997+Klopotek0@users.noreply.github.com>
2023-11-27 21:33:13 +0100

    Update test.py

--- test.py
+++ test.py
@@ -84,18 +84,18 @@ def main():
 
     vgg = net.vgg
     vgg.load_state_dict(torch.load(args['vgg']))
-    encoder = net.Net(vgg).cuda()    
-
+    encoder = net.Net(vgg)   
+    counter=0
     # model = model.to(device)
-    model.cuda()
     # model = nn.DataParallel(model)
     model.eval()
     for batch_id, (imgA, imgB, name) in enumerate(test_loader):
-        base_code = encoder.cat_tensor(imgB.cuda())
-        stylized = model(imgA.cuda(),domain_class=base_code.cuda())
+        base_code = encoder.cat_tensor(imgB)
+        stylized = model(imgA,domain_class=base_code)
         stylized = torch.clamp(stylized,0,1)
         output_name = os.path.join(args['output'], args['task_name'],'img_gen', name[0])
         save_image(stylized.cpu(), output_name, nrow=1)
-
+        counter=counter+1
+        print(counter)
 if __name__ == "__main__":
     main()
